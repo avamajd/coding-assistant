ebd0b18f0a67e17f58b15761d0e42b1e0f4e03d6
ava <47380749+avamajd@users.noreply.github.com>
2026-09-18 22:05:29 +0330

    Added Fibonacci function to generate sequence up to n terms

--- fibonacci.ts
+++ fibonacci.ts
@@ -1,1 +1,21 @@
-This file has been deleted.
+/**
+ * Function to generate Fibonacci sequence up to n terms
+ * @param n - number of terms to generate
+ * @returns Array of Fibonacci numbers
+ */
+function fibonacci(n: number): number[] {
+    const fib: number[] = [];
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            fib.push(0);
+        } else if (i === 1) {
+            fib.push(1);
+        } else {
+            fib.push(fib[i - 1] + fib[i - 2]);
+        }
+    }
+    return fib;
+}
+
+// Example usage:
+console.log(fibonacci(10));
