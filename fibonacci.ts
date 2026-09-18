25d41ff302c03499c63b69d317ed7372247a372c
ava <47380749+avamajd@users.noreply.github.com>
2026-09-18 21:52:30 +0330

    Delete fibonacci.ts file

--- fibonacci.ts
+++ fibonacci.ts
@@ -1,16 +1,1 @@
-function fibonacci(n: number): number[] {
-    const fib: number[] = [];
-    for (let i = 0; i < n; i++) {
-        if (i === 0) {
-            fib.push(0);
-        } else if (i === 1) {
-            fib.push(1);
-        } else {
-            fib.push(fib[i - 1] + fib[i - 2]);
-        }
-    }
-    return fib;
-}
-
-// Example usage:
-console.log(fibonacci(10)); // Outputs the first 10 Fibonacci numbers
+This file has been deleted.
